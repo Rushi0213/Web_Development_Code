e7336876af3c75e44fb4ee822ef7f345fa210155
Rushi0213 <156175551+Rushi0213@users.noreply.github.com>
2026-04-16 00:16:39 +0530

    datatypes of javascript with html file

---
+++ JavaScript/datatype.js
@@ -0,0 +1,87 @@
+//string datatype
+
+let a="forTUne";
+
+console.log(a);
+console.log(typeof(a));
+
+let b=a.charAt(4);// print  selected letter
+console.log(b);
+
+let c=a.charCodeAt(4)//  print the letter of four no of fortune
+console.log(c);
+
+
+let d=a.startsWith('F')
+console.log(d);
+
+let e=a.endsWith('e')
+console.log(e);
+
+let f=a.includes('o')
+console.log(f);
+
+let g=a.toUpperCase()
+console.log(g);
+
+let h=a.toLowerCase();
+console.log(h);
+
+let i=a.concat(" cloud")
+console.log(i);
+
+let j=a.replace("forTUne"," Cravita")
+console.log(j);
+
+let k=a.repeat(5)
+console.log( k);
+
+
+// number datatype
+
+let ab =2345;
+
+console.log(ab);
+console.log(typeof(ab));
+
+let n=ab.toFixed(4);// add more zero
+console.log(n);
+
+let o=ab.toString()// convert in to string
+console.log(o);
+console.log(typeof(o));
+
+//boolean datatype
+
+let bc= true;
+console.log(bc);
+console.log(typeof(bc));
+
+
+// undefined
+
+let q;
+console.log(q);
+console.log(typeof(q));
+
+
+
+// null datatype
+
+let r=null; // object type datatype
+console.log(r);
+console.log(typeof(r));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
