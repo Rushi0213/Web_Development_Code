c705d25fc5c3b565a174248e18ee5407cc2af9c1
Rushi0213 <156175551+Rushi0213@users.noreply.github.com>
2026-04-18 00:35:05 +0530

    control statement second type looping statement

---
+++ JavaScript/looping.js
@@ -0,0 +1,39 @@
+ //looping statememnt
+
+ //1) for loop
+ // when we konw the count of repetation then use
+
+ let i;
+ for( i=0;i<10;i++)
+ {
+    console.log("hello javascript",i);
+    
+ }
+ console.log("");
+ 
+
+ //2) whiel loop
+
+
+ let a=1;
+
+ while(a<=10)
+ {
+    console.log("hello while loop",a);
+    a++;
+    
+ }
+ console.log("");
+ 
+ //3) do while loop
+
+ let x=1;
+ do
+ {
+     
+    console.log("hello do",x);
+     x++;
+    
+ }
+ while(x<=10)
+  
